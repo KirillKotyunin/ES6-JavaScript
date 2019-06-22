1b82f76ae166c7978829d3aaaa12dd630a5ed6b3
Kirill <kotyunin.k@mail.ru>
2019-06-22 17:34:26 +0600

    Lesson about 'const' variable

---
+++ dist/scripts/const.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// console.log(PI); is not defined
+// const PI; error
+var PI = 3.14159;
+// PI = 15; error
+
+console.log(PI);
+
+var OBJECT = {
+	PROP: 3.14159
+};
+
+console.log(OBJECT.PROP);
---
+++ src/const.js
@@ -0,0 +1,12 @@
+// console.log(PI); is not defined
+// const PI; error
+const PI = 3.14159;
+// PI = 15; error
+
+console.log(PI);
+
+const OBJECT = {
+	PROP: 3.14159
+};
+
+console.log(OBJECT.PROP);
