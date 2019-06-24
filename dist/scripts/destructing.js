cf8d64848acf5f8f318674a5353398eef239e076
Kirill <kotyunin.k@mail.ru>
2019-06-24 07:49:46 +0600

    Lesson about destructing in Array and Object

--- dist/scripts/destructing.js
+++ dist/scripts/destructing.js
@@ -42,3 +42,50 @@ no = _ref3[1];
 
 console.log('No is ', yes);
 console.log('Yes is ', no);
+
+var user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  social: {
+    facebook: 'johndoe',
+    twitter: 'jdoe'
+  }
+};
+
+var first = user.firstName,
+    last = user.lastName,
+    fb = user.social.facebook,
+    _user$age = user.age,
+    age = _user$age === undefined ? 25 : _user$age;
+
+
+console.log(first, last, fb, age);
+
+function post(url, _ref4) {
+  var _ref4$data = _ref4.data,
+      firstName = _ref4$data.firstName,
+      lastName = _ref4$data.lastName,
+      cache = _ref4.cache;
+
+  console.log(firstName, lastName, cache);
+}
+
+var result = post('api/users', { data: user, cache: false });
+
+function getUserInfo() {
+  return {
+    firstname: 'John',
+    lastname: 'Doe',
+    social: {
+      facebook: 'johndoe',
+      twitter: 'jdoe'
+    }
+  };
+}
+
+var _getUserInfo = getUserInfo(),
+    firstname = _getUserInfo.firstname,
+    lastname = _getUserInfo.lastname,
+    twitter = _getUserInfo.social.twitter;
+
+console.log(fistname, lastname, twitter);
--- src/destructing.js
+++ src/destructing.js
@@ -26,3 +26,38 @@ let no = 'No';
 [yes, no] = [no, yes];
 console.log('No is ', yes);
 console.log('Yes is ', no);
+
+let user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  social: {
+    facebook: 'johndoe',
+    twitter: 'jdoe'
+  }
+};
+
+let {firstName: first, lastName: last, social: {facebook: fb}, age = 25} = user;
+
+console.log(first, last, fb, age);
+
+function post(url, {data: {firstName, lastName}, cache}) {
+  console.log(firstName, lastName, cache);
+}
+
+let result = post('api/users', {data: user, cache: false});
+
+function getUserInfo() {
+  return {
+    firstname: 'John',
+    lastname: 'Doe',
+    social: {
+      facebook: 'johndoe',
+      twitter: 'jdoe'
+    }
+  };
+}
+
+let {firstname, lastname, social: {twitter}} = getUserInfo();
+
+
+console.log(fistname, lastname, twitter);
